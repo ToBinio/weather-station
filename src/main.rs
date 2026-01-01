22d73532cd89aa9dca0f0b934405c7692931f6d2
tobinio <Tobias.frischmann1@gmail.com>
2026-01-01 09:34:30 +0100

    run async where possible

--- src/main.rs
+++ src/main.rs
@@ -7,29 +7,25 @@
 )]
 #![deny(clippy::large_stack_frames)]
 
-use core::cell::RefCell;
-
-use alloc::rc::Rc;
 use alloc::string::ToString;
-use bosch_bme680::{Bme680, DeviceAddress};
+use bosch_bme680::{AsyncBme680, DeviceAddress};
+use embassy_embedded_hal::shared_bus::asynch::i2c::I2cDevice;
 use embassy_executor::Spawner;
 use embassy_sync::blocking_mutex::raw::NoopRawMutex;
 use embassy_sync::channel::{Channel, Receiver, Sender};
+use embassy_sync::mutex::Mutex;
 use embassy_time::{Delay, Duration, Timer};
-use embedded_hal_bus::i2c::RcDevice;
 use esp_backtrace as _;
-use esp_hal::Blocking;
+use esp_hal::Async;
 use esp_hal::clock::CpuClock;
 use esp_hal::gpio::{Input, InputConfig};
 use esp_hal::i2c::master::{Config as I2cConfig, I2c};
 use esp_hal::timer::timg::TimerGroup;
-use lcd_lcm1602_i2c::sync_lcd::Lcd;
-use scd4x::Scd4x;
+use lcd_lcm1602_i2c::async_lcd::Lcd;
+use scd4x::Scd4xAsync;
 use static_cell::StaticCell;
 
 extern crate alloc;
-
-//TODO: make full async - https://github.com/embassy-rs/embassy/blob/main/examples/rp/src/bin/shared_bus.rs
 
 // This creates a default app-descriptor required by the esp-idf bootloader.
 // For more information see: <https://docs.espressif.com/projects/esp-idf/en/stable/esp32/api-reference/system/app_image_format.html#application-description>
@@ -58,7 +54,11 @@ pub enum Events {
     ButtonPress,
 }
 
-static CHANNEL: StaticCell<Channel<NoopRawMutex, Events, 1>> = StaticCell::new();
+type I2c1BusV33 = Mutex<NoopRawMutex, I2c<'static, Async>>;
+
+type EventChannel = Channel<NoopRawMutex, Events, 1>;
+type EventSender = Sender<'static, NoopRawMutex, Events, 1>;
+type EventReceiver = Receiver<'static, NoopRawMutex, Events, 1>;
 
 #[allow(
     clippy::large_stack_frames,
@@ -95,27 +95,25 @@ async fn main(spawner: Spawner) {
     let i2c_v33 = I2c::new(peripherals.I2C0, I2cConfig::default())
         .unwrap()
         .with_scl(peripherals.GPIO22)
-        .with_sda(peripherals.GPIO21);
-    let shared_i2c_v33 = Rc::new(RefCell::new(i2c_v33));
+        .with_sda(peripherals.GPIO21)
+        .into_async();
+    static I2C_V33_BUS: StaticCell<I2c1BusV33> = StaticCell::new();
+    let i2c_v33_bus = I2C_V33_BUS.init(Mutex::new(i2c_v33));
 
     let i2c_v5 = I2c::new(peripherals.I2C1, I2cConfig::default())
         .unwrap()
         .with_scl(peripherals.GPIO26)
-        .with_sda(peripherals.GPIO25);
+        .with_sda(peripherals.GPIO25)
+        .into_async();
 
+    static CHANNEL: StaticCell<EventChannel> = StaticCell::new();
     let event_channel = CHANNEL.init(Channel::new());
 
     spawner
-        .spawn(read_bme_680(
-            RcDevice::new(shared_i2c_v33.clone()),
-            event_channel.sender(),
-        ))
+        .spawn(read_bme_680(i2c_v33_bus, event_channel.sender()))
         .unwrap();
     spawner
-        .spawn(read_scd_41(
-            RcDevice::new(shared_i2c_v33.clone()),
-            event_channel.sender(),
-        ))
+        .spawn(read_scd_41(i2c_v33_bus, event_channel.sender()))
         .unwrap();
     spawner
         .spawn(listen_button(button_pin, event_channel.sender()))
@@ -128,23 +126,18 @@ async fn main(spawner: Spawner) {
 }
 
 #[embassy_executor::task]
-async fn read_bme_680(
-    i2c: RcDevice<I2c<'static, Blocking>>,
-    sender: Sender<'static, NoopRawMutex, Events, 1>,
-) {
-    let mut sensor = Bme680::new(
-        i2c,
-        DeviceAddress::Primary,
-        Delay,
-        &bosch_bme680::Configuration::default(),
-        20,
-    )
-    .unwrap();
+async fn read_bme_680(i2c_bus: &'static I2c1BusV33, sender: EventSender) {
+    let i2c = I2cDevice::new(i2c_bus);
+    let mut sensor = AsyncBme680::new(i2c, DeviceAddress::Primary, Delay, 20);
+    sensor
+        .initialize(&bosch_bme680::Configuration::default())
+        .await
+        .unwrap();
 
     loop {
         Timer::after(Duration::from_secs(5)).await;
 
-        let data = sensor.measure().unwrap();
+        let data = sensor.measure().await.unwrap();
         sender
             .send(Events::BME680 {
                 temp: data.temperature,
@@ -157,20 +150,18 @@ async fn read_bme_680(
 }
 
 #[embassy_executor::task]
-async fn read_scd_41(
-    i2c: RcDevice<I2c<'static, Blocking>>,
-    sender: Sender<'static, NoopRawMutex, Events, 1>,
-) {
-    let mut sensor = Scd4x::new(i2c, Delay);
-    sensor.wake_up();
-    sensor.stop_periodic_measurement().unwrap();
-    sensor.reinit().unwrap();
+async fn read_scd_41(i2c_bus: &'static I2c1BusV33, sender: EventSender) {
+    let i2c = I2cDevice::new(i2c_bus);
+    let mut sensor = Scd4xAsync::new(i2c, Delay);
+    sensor.wake_up().await;
+    sensor.stop_periodic_measurement().await.unwrap();
+    sensor.reinit().await.unwrap();
 
-    sensor.start_periodic_measurement().unwrap();
+    sensor.start_periodic_measurement().await.unwrap();
     loop {
         Timer::after(Duration::from_secs(5)).await;
 
-        let data = sensor.measurement().unwrap();
+        let data = sensor.measurement().await.unwrap();
         sender
             .send(Events::SCD41 {
                 co2: data.co2,
@@ -182,7 +173,7 @@ async fn read_scd_41(
 }
 
 #[embassy_executor::task]
-async fn listen_button(mut pin: Input<'static>, sender: Sender<'static, NoopRawMutex, Events, 1>) {
+async fn listen_button(mut pin: Input<'static>, sender: EventSender) {
     loop {
         pin.wait_for_high().await;
         pin.wait_for_low().await;
@@ -213,44 +204,22 @@ enum DisplayMode {
 }
 
 impl DisplayMode {
-    fn display(&self, data: &Data, lcd: &mut Lcd<'_, I2c<'static, Blocking>, Delay>) {
-        match self {
-            DisplayMode::Temperature => {
-                lcd.clear().unwrap();
-                lcd.write_str("Temperature").unwrap();
-                lcd.set_cursor(1, 0).unwrap();
-                lcd.write_str(&data.temp.to_string()).unwrap();
-                lcd.write_str("C").unwrap();
-            }
-            DisplayMode::Humidity => {
-                lcd.clear().unwrap();
-                lcd.write_str("Humidity").unwrap();
-                lcd.set_cursor(1, 0).unwrap();
-                lcd.write_str(&data.humidity.to_string()).unwrap();
-                lcd.write_str("%").unwrap();
-            }
-            DisplayMode::Pressure => {
-                lcd.clear().unwrap();
-                lcd.write_str("Pressure").unwrap();
-                lcd.set_cursor(1, 0).unwrap();
-                lcd.write_str(&data.pressure.to_string()).unwrap();
-                lcd.write_str("hPa").unwrap();
-            }
+    async fn display(&self, data: &Data, lcd: &mut Lcd<'_, I2c<'static, Async>, Delay>) {
+        let (header, value, unit) = match self {
+            DisplayMode::Temperature => ("Temperature", &data.temp.to_string(), "C"),
+            DisplayMode::Humidity => ("Humidity", &data.humidity.to_string(), "%"),
+            DisplayMode::Pressure => ("Pressure", &data.pressure.to_string(), "hPa"),
+            DisplayMode::CO2 => ("CO2", &data.co2.to_string(), "ppm"),
             DisplayMode::GasResistance => {
-                lcd.clear().unwrap();
-                lcd.write_str("Gas Resistance").unwrap();
-                lcd.set_cursor(1, 0).unwrap();
-                lcd.write_str(&data.gas_resistance.to_string()).unwrap();
-                lcd.write_str("Ohm").unwrap();
+                ("Gas Resistance", &data.gas_resistance.to_string(), "Ohm")
             }
-            DisplayMode::CO2 => {
-                lcd.clear().unwrap();
-                lcd.write_str("CO2").unwrap();
-                lcd.set_cursor(1, 0).unwrap();
-                lcd.write_str(&data.co2.to_string()).unwrap();
-                lcd.write_str("ppm").unwrap();
-            }
-        }
+        };
+
+        lcd.clear().await.unwrap();
+        lcd.write_str(header).await.unwrap();
+        lcd.set_cursor(1, 0).await.unwrap();
+        lcd.write_str(value).await.unwrap();
+        lcd.write_str(unit).await.unwrap();
     }
 
     fn next(self) -> DisplayMode {
@@ -265,10 +234,7 @@ impl DisplayMode {
 }
 
 #[embassy_executor::task]
-async fn display_data(
-    mut i2c: I2c<'static, Blocking>,
-    receiver: Receiver<'static, NoopRawMutex, Events, 1>,
-) {
+async fn display_data(mut i2c: I2c<'static, Async>, receiver: EventReceiver) {
     let mut data = Data {
         temp: 0.0,
         humidity: 0.0,
@@ -284,9 +250,10 @@ async fn display_data(
         .with_cursor_on(false)
         .with_rows(2)
         .init()
+        .await
         .unwrap();
 
-    mode.display(&data, &mut lcd);
+    mode.display(&data, &mut lcd).await;
 
     loop {
         match receiver.receive().await {
@@ -309,6 +276,6 @@ async fn display_data(
             }
         }
 
-        mode.display(&data, &mut lcd);
+        mode.display(&data, &mut lcd).await;
     }
 }
